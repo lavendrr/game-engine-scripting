8f03fa3a691b05070c0684e37698298431449a67
Ricardo Moctezuma <rmoctezumaf@gmail.com>
2024-03-10 18:52:41 -0500

    +Door Animation

--- Assets/Maze/Scripts/Door.cs
+++ Assets/Maze/Scripts/Door.cs
@@ -7,12 +7,14 @@ public class Door : MonoBehaviour
 {
     private bool isOpen = false;
     private SpriteRenderer spriteRenderer;
+    private Animator animator;
     [SerializeField]
     private Sprite openSprite;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     public bool OpenDoor(int keys)
@@ -23,7 +25,8 @@ public class Door : MonoBehaviour
             {
                 gameObject.GetComponent<BoxCollider2D>().enabled = false;
                 isOpen = true;
-                spriteRenderer.sprite = openSprite;
+                //spriteRenderer.sprite = openSprite;
+                animator.SetTrigger("OpenDoor");
                 return true;
             }
             else
