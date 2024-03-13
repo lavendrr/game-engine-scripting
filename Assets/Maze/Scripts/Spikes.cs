e6b12fa7a3d13fed089966183adb5e2487abd2c4
Ricardo Moctezuma <rmoctezumaf@gmail.com>
2024-03-12 19:27:08 -0500

    Updated Spike damage to 1

--- Assets/Maze/Scripts/Spikes.cs
+++ Assets/Maze/Scripts/Spikes.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Spikes : MonoBehaviour
 {
     [SerializeField]
-    private int damageAmount = 2;
+    private int damageAmount = 1;
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         other.gameObject.GetComponent<Player>().Damage(damageAmount);
-        //gameObject.SetActive(false);
     }
 }
